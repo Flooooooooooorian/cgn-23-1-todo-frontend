76f761855bdb7aa599db6ef76508cb2326f0626a
mboosch <matthias.boosch@neuefische.de>
2023-03-24 10:11:36 +0100

    Integrate axios requests in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import TodoBoards from "./components/TodoBoards";
 import AddTodo from "./components/AddTodo";
-import useTodos from "./hooks/useTodos";
+import {NewTodo, Todo} from "./model/Todo";
+import axios from "axios";
 
 function App() {
 
-    const {todos, addTodo, updateTodo} = useTodos()
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    useEffect(() => {
+        loadAllTodos()
+    }, [])
+
+    function loadAllTodos() {
+        return axios.get("/api/todo")
+            .then(response => response.data)
+            .then(setTodos)
+            .catch(console.error)
+    }
+
+    function addTodo(todo: NewTodo) {
+        return axios.post("/api/todo", todo)
+            .then(response => response.data)
+            .then(data => setTodos(prevState => [...prevState, data]))
+    }
+
+    function updateTodo(todo: Todo) {
+        axios.put("/api/todo/" + todo.id, todo)
+            .then(response => response.data)
+            .then(data =>  setTodos(prevState => {
+                return prevState.map(currentTodo => {
+                    if (currentTodo.id === todo.id) {
+                        return data
+                    }
+                    return currentTodo
+                })
+            }))
+    }
 
     return (
         <div className="App">
